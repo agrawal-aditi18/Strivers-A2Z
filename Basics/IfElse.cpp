765a52978071c57f204c1a1655e756058449712f
agrawal-aditi18 <aditiagrawalsofficial@gmail.com>
2025-12-10 11:25:45 +0530

    added ifelse file

---
+++ Basics/IfElse.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int age;
+    cout << "Enter you age: ";
+    cin >> age;
+
+    if(age >= 18)
+    cout << "You are an adult.";
+    else
+        cout << "You are not an adult.";
+    return 0;
+}

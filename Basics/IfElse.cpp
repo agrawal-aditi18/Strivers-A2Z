8e78fbd652cbdc92f454e23be814a6e7da116729
agrawal-aditi18 <aditiagrawalsofficial@gmail.com>
2025-12-10 16:58:59 +0530

    updated

--- Basics/IfElse.cpp
+++ Basics/IfElse.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int age;
-    cout << "Enter you age: ";
-    cin >> age;
+    int marks;
+    cout << "Enter you marks: ";
+    cin >> marks;
 
-    if(age >= 18)
-    cout << "You are an adult.";
+    if(80 <= marks && marks <= 100) cout << "Grade A"<<endl;
+    else if(60 <= marks) {cout << "Grade B"<<endl;}
+    else if(50 <= marks) cout << "Grade C"<<endl;
+    else if(45 <= marks) cout << "Grade D"<<endl;
+    else if(25 <= marks) cout << "Grade E"<<endl;
     else
-        cout << "You are not an adult.";
+    cout << "Grade F";
     return 0;
 }

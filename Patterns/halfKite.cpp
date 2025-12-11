22d0473b1458ae3fc045ddafe8a041dbfd4689c4
agrawal-aditi18 <aditiagrawalsofficial@gmail.com>
2025-12-11 19:22:46 +0530

    updated logic

--- Patterns/halfKite.cpp
+++ Patterns/halfKite.cpp
@@ -13,7 +13,7 @@ void halfUp(){
 void halfdown(){
     for (int i = 1; i <= 5; i++)
     {
-        for (int j = 0; j < 5 - i + 1; j++)
+        for (int j = 1; j < 5 - i + 1; j++)
         {
             cout << "*" ;
         }
@@ -24,4 +24,20 @@ void halfdown(){
 int main(){
     halfUp();
     halfdown();
+}
+
+//OR
+
+#include <iostream>
+using namespace std;
+
+int main(){
+   for(int i =1 ; i<=2*5-1; i++){
+    int stars = i;
+    if(i>5) stars = 2*5-i;
+    for(int j = 1; j<=stars; j++){
+        cout << "*";
+    }
+    cout << endl;
+}
 }
